9dbda2a9d7ae81248b9af7f8a4fee2c8003040d6
harmo <dtharmo@gmail.com>
2015-09-22 21:10:57 +0200

    Add Debug settings

--- dicodigital/settings.py
+++ dicodigital/settings.py
@@ -90,3 +90,40 @@ STATICFILES_DIRS = (project_path('static'), )
 MEDIA_ROOT = project_path('public', 'media')
 MEDIA_URL = '/media/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+
+# Logging
+if DEBUG:
+    try:
+        import debug_toolbar  # NOQA
+    except ImportError:
+        pass
+    else:
+        INTERNAL_IPS = (
+            '127.0.0.1',
+        )
+
+        INSTALLED_APPS += (
+            'debug_toolbar',
+        )
+
+        MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+            'debug_toolbar.middleware.DebugToolbarMiddleware',
+        )
+
+        DEBUG_TOOLBAR_CONFIG = {
+            'INTERCEPT_REDIRECTS': False,
+            'HIDE_DJANGO_SQL': False,
+        }
+
+        DEBUG_TOOLBAR_PANELS = (
+            'debug_toolbar.panels.timer.TimerDebugPanel',
+            'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+            'debug_toolbar.panels.template.TemplateDebugPanel',
+            'debug_toolbar.panels.sql.SQLDebugPanel',
+            'debug_toolbar.panels.signals.SignalDebugPanel',
+            # 'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+            # 'debug_toolbar.panels.headers.HeaderDebugPanel',
+            # 'debug_toolbar.panels.version.VersionDebugPanel',
+            # 'debug_toolbar.panels.logger.LoggingPanel',
+        )
