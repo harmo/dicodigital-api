5b8db125bc7ac956647f562a14972c34e0f18dbe
harmo <dtharmo@gmail.com>
2015-09-22 21:07:33 +0200

    Add ENV database to settings

--- dicodigital/settings.py
+++ dicodigital/settings.py
@@ -1,4 +1,6 @@
 import os
+import dj_database_url
+
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 project_path = lambda *a: os.path.join(BASE_DIR, *a)
@@ -64,10 +66,7 @@ WSGI_APPLICATION = 'dicodigital.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
