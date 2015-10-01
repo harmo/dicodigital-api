5673976f7b9eb672c016965129455d85c0f467a1
harmo <dtharmo@gmail.com>
2015-10-01 22:59:44 +0200

    Add 403 permission test during anonymous post

--- dicodigital/dico/tests.py
+++ dicodigital/dico/tests.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class AnonymousTest(TestCase):
+
+    def setUp(self):
+        super(AnonymousTest, self).setUp()
+        self.user = get_user_model().objects.create(username='test_user')
+        self.c = APIClient()
+
+    def test_cant_post(self):
+        response = self.c.post('/word/')
+        self.assertEqual(response.status_code, 403)
--- dicodigital/dico/views.py
+++ dicodigital/dico/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from . import serializers, models
 
 
@@ -7,9 +7,11 @@ class Word(viewsets.ModelViewSet,
            generics.DestroyAPIView):
     queryset = models.Word.objects.all()
     serializer_class = serializers.Word
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class Definition(viewsets.ModelViewSet,
                  generics.DestroyAPIView):
     queryset = models.Definition.objects.all()
     serializer_class = serializers.Definition
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
