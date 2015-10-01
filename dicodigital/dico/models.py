ace5a15a3eb2ed98fba1ac6bced1d9b5e4f7847c
harmo <dtharmo@gmail.com>
2015-10-01 21:30:07 +0200

    Add & Definition models + migration

--- dicodigital/dico/models.py
+++ dicodigital/dico/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Word(models.Model):
+    label = models.CharField(max_length=128)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.label
+
+
+class Definition(models.Model):
+    contributor = models.ForeignKey(settings.AUTH_USER_MODEL)
+    text = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    word = models.ForeignKey(Word)
+    is_primary = models.BooleanField(default=False)
