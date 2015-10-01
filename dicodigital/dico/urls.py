9ea7097840052d6c0d64d252d2ac6a5bd308cecf
harmo <dtharmo@gmail.com>
2015-10-01 21:45:26 +0200

    Add some indent because indent is life

--- dicodigital/dico/urls.py
+++ dicodigital/dico/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework'))
 ]
