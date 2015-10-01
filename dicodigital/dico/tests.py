b75a7b8d15200c6ebc7cc8c16bc54c1d3c3deba4
harmo <dtharmo@gmail.com>
2015-10-01 23:19:38 +0200

    Add basic auth test for post/get requests

--- dicodigital/dico/tests.py
+++ dicodigital/dico/tests.py
@@ -4,13 +4,36 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 
 
-class AnonymousTest(TestCase):
+class TestUtils(TestCase):
 
     def setUp(self):
-        super(AnonymousTest, self).setUp()
-        self.user = get_user_model().objects.create(username='test_user')
+        super(TestUtils, self).setUp()
+        self.user = get_user_model().objects.create(
+            username='test_user', password='test')
         self.c = APIClient()
+
+
+class AnonymousTest(TestUtils):
 
     def test_cant_post(self):
         response = self.c.post('/word/')
         self.assertEqual(response.status_code, 403)
+
+    def test_can_list(self):
+        response = self.c.get('/word/')
+        self.assertEqual(response.status_code, 200)
+
+
+class ConnectedTest(TestUtils):
+
+    def setUp(self):
+        super(ConnectedTest, self).setUp()
+        self.c.force_authenticate(user=self.user)
+
+    def test_can_post_after_login(self):
+        response = self.c.post('/word/')
+        self.assertEqual(response.status_code, 400)
+
+    def test_can_list_after_login(self):
+        response = self.c.get('/word/')
+        self.assertEqual(response.status_code, 200)
