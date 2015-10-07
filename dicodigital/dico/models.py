68e2d5f66918e9b2a9292d89f386998c2d952f44
harmo <dtharmo@gmail.com>
2015-10-07 23:09:13 +0200

    Add django-autoslug
    This commit include the requirements and the new field
    in Word model (don't forget to migrate).
    
    The migration include the update of each existing objects in database
    to fill auto-slug field with slugified label.

--- dicodigital/dico/models.py
+++ dicodigital/dico/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from autoslug import AutoSlugField
 
 
 class Word(models.Model):
     label = models.CharField(max_length=128)
+    slug = AutoSlugField(populate_from='label')
     creator = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_at = models.DateTimeField(auto_now_add=True)
 
