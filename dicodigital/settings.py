5ae8585f906835b960a1e323f6f6faf9624c7ee2
Laurent Saint-Félix <anaethelion@gmail.com>
2015-10-10 23:29:15 +0200

    Add pagination to improve performances on lists. We may have to switch to cursors pretty soon.

--- dicodigital/settings.py
+++ dicodigital/settings.py
@@ -103,6 +103,8 @@ STATICFILES_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
 }
 
 
