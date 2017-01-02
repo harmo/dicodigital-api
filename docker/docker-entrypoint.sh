f12a1810c3d373a720d0420b5230aadbe65647aa
harmo <dtharmo@gmail.com>
2017-01-02 23:04:34 +0100

    Improve dockerization with docker-compose

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+source /venv/bin/activate
+cd /usr/src/app/
+
+if [ -e '.env' ]; then
+    source .env
+fi
+
+DIRECT_MANAGE_COMMANDS=(
+    'createsuperuser'
+    'help'
+    'migrate'
+    'makemigrations'
+    'runserver'
+    'loaddata'
+)
+
+if [ -z "$SECRET_KEY" ]; then
+    export SECRET_KEY=secret
+fi
+if [ -z "$DJANGO_SETTINGS_MODULE" ]; then
+    export DJANGO_SETTINGS_MODULE=dicodigital.settings
+fi
+if [ -z "$DEBUG" ]; then
+    export DEBUG=True
+fi
+
+for COMMAND in ${DIRECT_MANAGE_COMMANDS[@]}; do
+    if [[ $COMMAND == $1 ]]; then
+        python manage.py $*
+        exit
+    fi
+done
+case $1 in
+    tests)
+        export DJANGO_SETTINGS_MODULE=tests.settings
+        export DATABASE_URL=sqlite:///:memory:
+        pytest -s
+        ;;
+    watch-tests)
+        shift
+        export DJANGO_SETTINGS_MODULE=tests.settings
+        export DATABASE_URL=sqlite:///:memory:
+        pytest -s $*
+        watchmedo shell-command \
+            --patterns="*.py" \
+            --recursive \
+            --command="pytest -s $*" \
+            --drop \
+            ./
+        ;;
+    *)
+        $*
+        ;;
+esac
