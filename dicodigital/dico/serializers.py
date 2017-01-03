a94640a43baa053975bdca47268b1e20309eaa2a
harmo <dtharmo@gmail.com>
2017-01-03 22:12:42 +0100

    Clean code in serializers

--- dicodigital/dico/serializers.py
+++ dicodigital/dico/serializers.py
@@ -9,7 +9,8 @@ class Definition(serializers.ModelSerializer):
     word = serializers.HyperlinkedRelatedField(
         view_name='word-detail',
         lookup_field='id',
-        read_only=True)
+        read_only=True
+    )
 
     class Meta:
         model = models.Definition
@@ -20,32 +21,40 @@ class Definition(serializers.ModelSerializer):
         word_id = validated_data.pop('word')
         word = models.Word.objects.get(id=word_id)
         is_primary = True
+
         if word.definitions.count() > 0:
             is_primary = False
+
         return models.Definition.objects.create(
             word=word,
             is_primary=is_primary,
-            **validated_data)
+            **validated_data
+        )
 
 
 class Word(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='word-detail',
         lookup_field='id',
-        read_only=True)
+        read_only=True
+    )
     definitions = Definition(many=True, required=False)
     creator = serializers.SlugRelatedField(
-        slug_field='username', read_only=True)
+        slug_field='username', read_only=True
+    )
 
     class Meta:
         model = models.Word
-        fields = ('id', 'label', 'creator', 'url',
-                  'created_at', 'definitions')
+        fields = (
+            'id', 'label', 'creator', 'url',
+            'created_at', 'definitions'
+        )
 
     def create(self, validated_data):
         definitions = []
         if 'definitions' in validated_data:
             definitions = validated_data.pop('definitions')
+
         word = models.Word.objects.create(**validated_data)
         for definition in definitions:
             is_primary = True if definition == definitions[0] else False
@@ -53,7 +62,9 @@ class Word(serializers.ModelSerializer):
                 word=word,
                 contributor=word.creator,
                 is_primary=is_primary,
-                **definition)
+                **definition
+            )
+
         return word
 
 
@@ -61,9 +72,12 @@ class Vote(serializers.ModelSerializer):
     definition = serializers.HyperlinkedRelatedField(
         view_name='definition-detail',
         lookup_field='id',
-        read_only=True)
+        read_only=True
+    )
 
     class Meta:
         model = models.Vote
-        fields = ('id', 'definition', 'score', 'created_at',
-                  'user', 'ip_address', 'cookie')
+        fields = (
+            'id', 'definition', 'score', 'created_at',
+            'user', 'ip_address', 'cookie'
+        )
