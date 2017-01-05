cadd9106f030db6bb8f8b6df2f9e9be9c21c518b
harmo <dtharmo@gmail.com>
2017-01-05 21:26:34 +0100

    Authorize anonymous user to vote

--- dicodigital/dico/models.py
+++ dicodigital/dico/models.py
@@ -29,7 +29,7 @@ class Definition(models.Model):
 
 
 class Vote(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     ip_address = models.GenericIPAddressField(null=True)
     cookie = models.CharField(max_length=64, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- dicodigital/dico/serializers.py
+++ dicodigital/dico/serializers.py
@@ -86,6 +86,9 @@ class Vote(serializers.ModelSerializer):
         definition_id = validated_data.pop('definition')
         definition = models.Definition.objects.get(id=definition_id)
 
+        if validated_data.get('user').is_anonymous():
+            validated_data['user'] = None
+
         return models.Vote.objects.create(
             definition=definition,
             **validated_data
