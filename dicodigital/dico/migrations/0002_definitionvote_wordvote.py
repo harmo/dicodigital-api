f1c52f3fb1556d03bda8c0575d025bf3e47f32fd
numahell <numahell@numajules.net>
2017-12-22 23:54:58 +0100

    Add on_delete in migrations steps

--- dicodigital/dico/migrations/0002_definitionvote_wordvote.py
+++ dicodigital/dico/migrations/0002_definitionvote_wordvote.py
@@ -21,8 +21,8 @@ class Migration(migrations.Migration):
                 ('cookie', models.CharField(blank=True, max_length=64, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('score', models.IntegerField()),
-                ('definition', models.ForeignKey(to='dico.Definition')),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+                ('definition', models.ForeignKey(to='dico.Definition', on_delete=models.CASCADE)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)),
             ],
             options={
                 'abstract': False,
@@ -36,8 +36,8 @@ class Migration(migrations.Migration):
                 ('cookie', models.CharField(blank=True, max_length=64, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('score', models.IntegerField()),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
-                ('word', models.ForeignKey(to='dico.Word')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)),
+                ('word', models.ForeignKey(to='dico.Word', on_delete=models.CASCADE)),
             ],
             options={
                 'abstract': False,
